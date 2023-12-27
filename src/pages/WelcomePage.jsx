85a3ceb33a9a6a5aa841f937eed9000394af656c
Adeolu Amole <adeoluamole@gmail.com>
2023-12-27 12:31:12 +0100

    Fix issue

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 const WelcomePage = () => {
   const backgroundImageUrl = 'url("images/Frame 1686551101.png")';
   return (
     <div
+      // Styling for the main container
       className="px-[32px] pt-[480px]"
       style={{
         backgroundImage: backgroundImageUrl,
@@ -15,24 +15,28 @@ const WelcomePage = () => {
         height: "100vh",
       }}
     >
-      <div className="w-[100%]  flex flex-col  gap-[26px] bg-white lg:bg-transparent">
-        <div className="w-[70px] h-[72px] flex flex-col items-center  mx-auto font-inter font-[700] text-[16px] ">
-          <img
-            src="images/bluelogo.png"
-            alt="bluelogo"
-            className="w-full"
-          />
+      {/* Content container */}
+      <div className="w-[100%] flex flex-col gap-[26px] bg-white lg:bg-transparent">
+        {/* Logo section */}
+        <div className="w-[70px] h-[72px] flex flex-col items-center mx-auto font-inter font-[700] text-[16px]">
+          <img src="images/bluelogo.png" alt="bluelogo" className="w-full" />
         </div>
+
+        {/* Buttons section */}
         <div className="flex flex-col gap-[26px] w-[364px] mx-auto">
-        <Link to="/login">
-        <button className="bg-[#0F515F] py-[19px] px-[147px] text-center rounded-[10px] text-white w-[100%]">
-            Login
-          </button>
-        </Link>
-          
-          <button className="bg-white py-[19px] px-[147px] text-center rounded-[10px] text-[#0F515F] border border-1 border-solid border-[#0F515F]">
-            Register
-          </button>
+          {/* Login button */}
+          <Link to="/login">
+            <button className="bg-[#0F515F] py-[19px] px-[147px] text-center rounded-[10px] text-white w-[100%]">
+              Login
+            </button>
+          </Link>
+
+          {/* Register button */}
+          <Link to="/register">
+            <button className="bg-white py-[19px] px-[147px] text-center rounded-[10px] text-[#0F515F] border border-1 border-solid border-[#0F515F]">
+              Register
+            </button>
+          </Link>
         </div>
       </div>
     </div>
